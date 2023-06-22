59195f3f278343f72de384a48307288c1330358e
Ilya <Dragoilya@yandex.ru>
2023-06-22 10:50:35 +0300

    Initialized data base

---
+++ db/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import User, Question, Event, Speech, Donation
+
+
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Event)
+admin.site.register(Speech)
+admin.site.register(Donation)
